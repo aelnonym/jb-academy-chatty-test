eb0153a7ed929456566f55060f10d84ef76367ac
Aelnonym <aelnonym@gmail.com>
2022-10-24 18:34:44 -0300

    Testing jetbrains academy

---
+++ task/bot/bot.py
@@ -0,0 +1,66 @@
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def ask_question(question, option_list, answer):
+    print(question)
+    for i in range(0, len(option_list)):
+        print(f"{i+1}. {option_list[i]}")
+    guess = int(input())
+    while guess != answer:
+        print("Please, try again.")
+        guess = int(input())
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    # write your code here
+    question = "Why do we use methods?"
+    option_list = ["To repeat a statement multiple times.",
+                   "To decompose a program into several small subroutines.",
+                   "To determine the execution time of a program.",
+                   "To interrupt the execution of a program."]
+    ask_question(question, option_list, 2)
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+# ...
+test()
+end()
